7df7c1ec10a080a466b403a8948ce10530e6432f
MarcusEvans03 <marcusevans03@gmail.com>
2018-03-05 09:03:38 -0600

    Todays Local Date

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
+import java.time.LocalDate;
+
 public class Main {
 
     public static void main(String[] args) {
 
         System.out.println("\n\nHello, AssignmentsApp!\n");
+
+        LocalDate todaysDate = todaysActualDate();
+        System.out.println("Todays Date is : " + todaysDate + "\n");
+
+        
+
+
+    }
+
+    private static LocalDate todaysActualDate() {
+        return LocalDate.now();
     }
 }
