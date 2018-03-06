4a2a1316fb536a2e7e7bc6a16b285b207fa43342
MarcusEvans03 <marcusevans03@gmail.com>
2018-03-05 21:24:08 -0600

    Output the number of days between two dates.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.Month;
@@ -22,10 +23,23 @@ public class Main {
         System.out.println("Todays date plus your specified number is " + todaysDatePlus + "\n");
 
         // Initialize a LocalDateTime object to your birthdate and the time 12:35 PM.
-        LocalDate birthDated = LocalDate.of(1998, Month.JANUARY,13);
+        LocalDate birthDated = LocalDate.of(1998, Month.JANUARY, 13);
         LocalDateTime howOldAmI = dateInitializer(birthDated);
         System.out.println("You are " + howOldAmI + " many days old\n");
 
+        // Output the day of the week (Sunday-Saturday) that you were born.
+        DayOfWeek dayOfWeek = whatDayOfTheWeek(birthDated);
+        System.out.println("The day of the week you were born was: " + dayOfWeek + "\n");
+
+        //Output the number of days you've been alive.
+        long howManyDaysOld = ageCounter(birthDated);
+        System.out.println("You are " + howManyDaysOld + " days old\n");
+
+        //TODO Output the number of days between two dates.
+        LocalDate firstDate = LocalDate.of(2018, Month.JANUARY, 1);
+        LocalDate secondDate = LocalDate.of(2018, Month.MARCH, 5);
+        long numberOfDays = daysBetween(firstDate, secondDate);
+        System.out.println("The number of days between your specified dates are " + numberOfDays + "\n");
 
 
     }
@@ -38,7 +52,7 @@ public class Main {
     }
 
 
-    private static String tomorrowsActualDate(){
+    private static String tomorrowsActualDate() {
         LocalDateTime today = LocalDateTime.now();
         LocalDateTime tommorowsDate = today.plusDays(1);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yy-MM-dd HH:mm:ss");
@@ -47,22 +61,43 @@ public class Main {
         return formatDateTime;
     }
 
-    private static LocalDate todaysDateAdder(int x){
+    private static LocalDate todaysDateAdder(int x) {
         LocalDate todaysDate = LocalDate.now();
         LocalDate dateAdder = todaysDate.plusWeeks(x);
         return dateAdder;
 
     }
+
     private static LocalDateTime dateInitializer(LocalDate birthDay) {
         LocalDate todaysDate = birthDay;
-        LocalDateTime today = todaysDate.atTime(12,35);
+        LocalDateTime today = todaysDate.atTime(12, 35);
 
 
         return today;
     }
 
+    private static DayOfWeek whatDayOfTheWeek(LocalDate birthDated) {
+        DayOfWeek dayOfTheWeek = birthDated.getDayOfWeek();
+        return dayOfTheWeek;
+    }
 
+    private static long ageCounter(LocalDate birthDay) {
+        LocalDate todaysDate = LocalDate.now();
 
+        long num1 = birthDay.toEpochDay();
+        long num2 = todaysDate.toEpochDay();
 
+        long daysOld = num2 - num1;
+        return daysOld;
+    }
+
+    private static long daysBetween(LocalDate firstDate, LocalDate secondDate) {
+        long dateOne = firstDate.toEpochDay();
+        long dateTwo = secondDate.toEpochDay();
+
+        long daysInBetween = dateTwo - dateOne;
+
+        return daysInBetween;
+    }
 }
 
